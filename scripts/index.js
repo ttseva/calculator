0803ed17f50b024d208a85d058263db87bb6cbfd
coder2 <coder2@email.com>
2025-02-08 01:51:58 +0300

    bugfix

--- scripts/index.js
+++ scripts/index.js
@@ -46,7 +46,8 @@ calculatorBody.addEventListener("click", (evt) => {
 
   function clearLastEntry() {
     if (screenText === "0") return;
-    if (screenText.slice(-2, -1) === ".") screenText = screenText.slice(0, -2);
+    if (/[\-\.]/.test(screenText.slice(-2, -1)))
+      screenText = screenText.slice(0, -2);
     else screenText = screenText.slice(0, -1);
     if (!screenText) screenText = "0";
 
@@ -62,32 +63,54 @@ calculatorBody.addEventListener("click", (evt) => {
     }
     if (!/\d$/.test(screenText)) screenText = screenText.slice(0, -1);
 
-    console.log(screenText);
+    console.log(screenText); //DON't FORGET TO DELETE
     outputScreenHistory.innerHTML = screenText + "=";
     outputScreen.textContent =
-      Math.round(calculateResult(screenText) * 100) / 100;
+      Math.round((calculateResult(screenText)) * 100) / 100;
   }
 
   function appendToOutput(char) {
     if (!screenText && isOperator(char)) return;
 
     const lastChar = screenText.slice(-1);
+    const secondLastChar = screenText.slice(-2, -1);
 
-    if (char === "0" || char === "00") {
-      if (lastChar === "0") return;
+    // предотвращение добавления нескольких нулей после оператора
+    if (
+      lastChar === "0" &&
+      screenText.length > 0 &&
+      isOperator(secondLastChar) &&
+      !isOperator(char)
+    ) {
+      return;
     }
 
+    // сокращение 00 до 0 в случае добавления после оператора
+    if (isOperator(lastChar) && char === "00") {
+      char = "0";
+    }
+
+    // обеспечение того, что в конце будет один оператор
     if (isOperator(char) && isOperator(lastChar)) {
       screenText = screenText.slice(0, -1) + char;
-    } else if (char === "00" && (!screenText || screenText === "0")) {
+    }
+
+    // предотвращение добавления нулей при пустой строке, установка в ноль при получении значени INFINITY/NAN
+    else if (
+      (char === "00" && screenText === "0") ||
+      screenText.match(/[a-zA-Z]+/)
+    ) {
       screenText = "0";
-    } else if (screenText === "0" && !isOperator(char)) {
+    }
+
+    // замена 0 на первую введенную цифру
+    else if (screenText === "0" && !isOperator(char)) {
       screenText = char;
     } else {
       screenText += char;
     }
 
-    outputScreen.textContent = screenText ? screenText : "&nbsp;";
+    outputScreen.textContent = screenText || "&nbsp;";
   }
 });
 
@@ -102,10 +125,8 @@ function calculateResult(expression) {
     } else {
       secondNumber = outputStack.pop();
       firstNumber = outputStack.pop();
-      console.log(firstNumber, secondNumber);
       if (secondNumber == "0" && char === "÷") return "ERROR";
       outputStack.push(operators[char](firstNumber, secondNumber));
-      console.log(char); //DON't FORGET TO DELETE
     }
   }
   return outputStack.pop();
