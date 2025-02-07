1b4dd091bb5a030db0f5a8341bbe214670c391dd
coder2 <coder2@email.com>
2025-02-07 20:20:14 +0300

    refactor output logic

--- scripts/index.js
+++ scripts/index.js
@@ -4,33 +4,58 @@ const outputScreen = document.querySelector(".calc-output__input");
 const outputScreenHistory = document.querySelector(".calc-output__equation");
 
 calculatorBody.addEventListener("click", (evt) => {
-  if (evt.target.classList.contains("calc-input__element")) {
-    let buttonText = evt.target.textContent; //ADD 'ADD TO STACK'
-    let screenText = outputScreen.textContent;
-    let screenHistoryText = outputScreenHistory.innerHTML;
-    
+  const target = evt.target;
+  if (!target.classList.contains("calc-input__element")) return;
 
-    if (buttonText == "AC") {
-      screenText = "";
-      screenHistoryText = "&nbsp;";
-    } else if (buttonText == "C") {
-      screenText = "";
-    } else if (buttonText == "=") {
-      if (/\d$/.test(screenText)) {
-        screenHistoryText = screenText;
-      } else {
-        screenHistoryText = screenText.slice(0, -1);
-      }
-      screenText = ""; //ADD FUNCTION OF COMPUTATION
-    } else {
-      screenText += buttonText;
-    }
+  let buttonText = target.textContent;
+  let screenText = outputScreen.textContent;
 
-    if (!screenHistoryText.trim()) {
-      screenHistoryText = "&nbsp;";
-    }
+  switch (buttonText) {
+    case "AC":
+      clearScreen(true);
+      break;
+    case "C":
+      clearScreen(false);
+      break;
+    case "=":
+      displayResult();
+      break;
+    case "POW":
+      appendToOutput("^");
+      break;
+    default:
+      appendToOutput(buttonText);
+  }
 
-    outputScreen.textContent = screenText;
-    outputScreenHistory.innerHTML = screenHistoryText;
+  function clearScreen(clearAll) {
+    outputScreen.textContent = "0";
+    if (clearAll) outputScreenHistory.innerHTML = "&nbsp;";
+  }
+
+  function displayResult() {
+    outputScreenHistory.innerHTML = /\d$/.test(screenText)
+      ? screenText
+      : screenText.slice(0, -1);
+    outputScreen.textContent = "";
+  }
+
+function appendToOutput(char) {
+  if (!screenText && isOperator(char)) return; 
+
+  if (isOperator(char) && isOperator(screenText.slice(-1))) {
+    screenText = screenText.slice(0, -1) + char; 
+  } else if (char === "00" && (!screenText || screenText === "0")) {
+    screenText = "0"; 
+  } else if (screenText === "0" && !isOperator(char)) {
+    screenText = char; 
+  } else {
+    screenText += char;
+  }
+
+  outputScreen.textContent = screenText ? screenText : "&nbsp;";
+}
+
+  function isOperator(char) {
+    return /[+\-×÷^]/.test(char);
   }
 });
